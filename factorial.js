cb88d15bc75dd9d7bdcf2c5456dac95e593e2418
Farahsaiza <farahsaiza@gmail.com>
2024-05-12 00:52:42 +0100

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,6 @@
+var n=parseInt(prompt("enter a number: "))
+        var f=1
+        for (let i = n ; i>0 ;  i--){
+           f= f *i
+        }
+        document.write("the factorial the number " + n + " is: "+f+"")
